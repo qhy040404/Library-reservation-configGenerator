70689e7d4b1ceb6f75d7836b3cd2880c248ac39c
qhy040404 <qhy040404@163.com>
2023-05-07 23:16:26 +0800

    Fix file lock

--- src/utils/GenerateFile.cs
+++ src/utils/GenerateFile.cs
@@ -4,8 +4,6 @@ namespace ConfigGenerator.src.utils
 {
     internal class GenerateFile
     {
-
-
         private static void InitializeFile()
         {
             string path = Path.Combine(Environment.CurrentDirectory, "config.conf");
@@ -13,18 +11,15 @@ namespace ConfigGenerator.src.utils
             {
                 File.Delete(path);
             }
-            File.Create(path);
+            File.Create(path).Close();
         }
         public static bool DataAlreadyExists()
         {
             int linesCount = 0;
-            string line;
             StreamReader sr = new("config.conf");
-            line = sr.ReadLine();
-            while (line != null)
+            while (sr.ReadLine() != null)
             {
                 linesCount++;
-                line = sr.ReadLine();
             }
             sr.Close();
             return linesCount != 1;
@@ -53,26 +48,26 @@ namespace ConfigGenerator.src.utils
 
         public static void WriteSeatsData(string seats)
         {
-            FileStream fsd = new("config.conf", FileMode.Append, FileAccess.Write);
-            TextWriter tsd = new StreamWriter(fsd, Encoding.Default);
-            StringBuilder ssd = new();
-            ssd.AppendLine(seats);
-            tsd.Write(ssd.ToString());
-            tsd.Flush();
-            tsd.Close();
-            fsd.Close();
+            FileStream fs = new("config.conf", FileMode.Append, FileAccess.Write);
+            TextWriter tw = new StreamWriter(fs, Encoding.Default);
+            StringBuilder sb = new();
+            sb.AppendLine(seats);
+            tw.Write(sb.ToString());
+            tw.Flush();
+            tw.Close();
+            fs.Close();
         }
 
         public static void WriteMailData(string mail, string mail_passwd)
         {
-            FileStream fmd = new("config.conf", FileMode.Append, FileAccess.Write);
-            TextWriter tmd = new StreamWriter(fmd, Encoding.Default);
-            StringBuilder smd = new();
-            smd.AppendLine(mail + " " + mail_passwd);
-            tmd.Write(smd.ToString());
-            tmd.Flush();
-            tmd.Close();
-            fmd.Close();
+            FileStream fs = new("config.conf", FileMode.Append, FileAccess.Write);
+            TextWriter tw = new StreamWriter(fs, Encoding.Default);
+            StringBuilder sb = new();
+            sb.AppendLine(mail + " " + mail_passwd);
+            tw.Write(sb.ToString());
+            tw.Flush();
+            tw.Close();
+            fs.Close();
         }
     }
 }
